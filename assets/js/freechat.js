3f0ec222f24c893d8bb6887d63d3d37969de0241
kerrongordon <kgpsounds.com@gmail.com>
2015-06-01 21:01:10 -0400

    added an register page

--- assets/js/freechat.js
+++ assets/js/freechat.js
@@ -1,8 +1,12 @@
 angular.module('freeChat', ["firebase"])
-	.controller('chatController', function($timeout) {
+	.controller('chatController', function($timeout, $firebaseObject, $firebaseArray, $firebaseAuth) {
 		var Data = this;		
 
 		var firebaseData = new Firebase("https://dazzling-fire-3713.firebaseio.com/freechatusers");
+
+		Data.data = $firebaseObject(firebaseData);
+
+		Data.messages = $firebaseArray(firebaseData);
 
 		Data.sendMessage = function() {
 			
@@ -19,4 +23,45 @@ angular.module('freeChat', ["firebase"])
 
 		
 
+	/*	firebaseData.authWithPassword({
+		  email    : "bobtony@firebase.com",
+		  password : "correcthorsebatterystaple"
+		}, function(error, userData) {
+		  if (error) {
+		    console.log("Error creating user:", error);
+		  } else {
+		    console.log("Successfully created user account with uid:", userData.uid);
+		  }
+		});*/
+	
+
+
+	})
+
+	.controller('usercontroller', function() {
+		var user = this;
+
+		var ref = new Firebase("https://dazzling-fire-3713.firebaseio.com/freechatusers");
+
+		//Creating User Accounts
+
+		user.create = function() {
+			ref.createUser({
+			  email    : user.userEamil,
+			  password : user.userPassword
+			}, function(error, userData) {
+			  if (error) {
+			    console.log("Error creating user:", error);
+			  } else {
+			    console.log("Successfully created user account with uid:", userData.uid);
+			  }
+			});
+
+			console.log('clicked');
+
+			user.userEamil = "";
+			user.userPassword = "";
+		}
+
+		
 	});
